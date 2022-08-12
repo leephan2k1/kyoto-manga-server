f6e3aafbe77e8f4ec098264afca36a3d5025653c
leephan2k1 <pttl2k1@gmail.com>
2022-08-12 21:04:12 +0700

    feat: 🎸 apply cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { PORT } from './configs';
 import routes from './routes';
 import dotenv from 'dotenv';
 import tasks from './services/cron.service';
+import cors from '@fastify/cors';
 dotenv.config();
 
 const server = fastify();
@@ -10,8 +11,14 @@ const server = fastify();
 //@ts-ignore
 server.register(routes, { prefix: '/api/v2' });
 
+server.register(cors, {
+    origin: ['http://localhost:3000', 'https://kyotomanga.live/'],
+});
+
 (async function () {
     try {
+        await server.ready();
+
         const address = await server.listen({ port: PORT });
         console.log(`Server listening at ${address}`);
 
